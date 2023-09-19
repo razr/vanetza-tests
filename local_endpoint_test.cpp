0bea9f3586dcb42d44b35231b2fc8780a29d788e
Andrei Kholodnyi <andrei.kholodnyi@windriver.com>
2023-09-19 15:30:22 +0200

    latest version

--- local_endpoint_test.cpp
+++ local_endpoint_test.cpp
@@ -4,6 +4,11 @@
 #include <netinet/in.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <netpacket/packet.h>
+
+#ifndef ETH_P_ALL
+#define ETH_P_ALL 0
+#endif
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -17,22 +22,30 @@ int main(int argc, char* argv[]) {
         boost::asio::io_service io_service;
         boost::asio::generic::raw_protocol raw_protocol(AF_PACKET, SOCK_RAW);
         boost::asio::generic::raw_protocol::socket raw_socket(io_service, raw_protocol);
-        boost::asio::generic::raw_protocol::endpoint endpoint = raw_socket.local_endpoint();
+
+        int local_socket = ::socket(AF_LOCAL, SOCK_DGRAM, 0);
+
+        ifreq request;
+
+        std::memset(&request, 0, sizeof(ifreq));
+        std::strncpy(request.ifr_name, interfaceName, IF_NAMESIZE);
+        request.ifr_name[IF_NAMESIZE - 1] = '\0';
+        ::ioctl(local_socket, SIOCGIFINDEX, &request);
+
+        sockaddr_ll socket_address = {0};
+        socket_address.sll_family = AF_PACKET;
+        socket_address.sll_protocol = htons(ETH_P_ALL);
+        socket_address.sll_ifindex = request.ifr_ifindex;
+
+        boost::asio::generic::raw_protocol::endpoint endpoint(&socket_address, sizeof(sockaddr_ll));
+        raw_socket.bind(endpoint);
+
+        boost::asio::generic::raw_protocol::endpoint local_endpoint = raw_socket.local_endpoint();
+
     } catch (const boost::system::system_error& e) {
-        // Handle the exception gracefully
         std::cerr << "Boost System Error: " << e.what() << std::endl;
         return 1;
     }
-
-    int local_socket = ::socket(AF_LOCAL, SOCK_DGRAM, 0);
-
-    ifreq request;
-
-    std::memset(&request, 0, sizeof(ifreq));
-    std::strncpy(request.ifr_name, interfaceName, IF_NAMESIZE);
-    request.ifr_name[IF_NAMESIZE - 1] = '\0';
-    ::ioctl(local_socket, SIOCGIFINDEX, &request);
-    std::cout << "Interface index for " << interfaceName << ": " << request.ifr_ifindex << std::endl;
 
     return 0;
 }
